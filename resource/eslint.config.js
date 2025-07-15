1719521c265fb974a2f6801239334cae5682a472
juneszh <juneszh@gmail.com>
2025-07-15 15:14:58 +0800

    chore: update resource

--- resource/eslint.config.js
+++ resource/eslint.config.js
@@ -2,34 +2,29 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default [
-    { ignores: ['dist', 'tinymce.copy.cjs'] },
-    {
-      files: ['**/*.{js,jsx}'],
-      languageOptions: {
-        ecmaVersion: 2020,
-        globals: globals.browser,
-        parserOptions: {
-          ecmaVersion: 'latest',
-          ecmaFeatures: { jsx: true },
-          sourceType: 'module',
-        },
-      },
-      plugins: {
-        'react-hooks': reactHooks,
-        'react-refresh': reactRefresh,
-      },
-      rules: {
-        ...js.configs.recommended.rules,
-        ...reactHooks.configs.recommended.rules,
-        'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-        'react-refresh/only-export-components': [
-          'warn',
-          { allowConstantExport: true },
-        ],
-        'react/prop-types': 'off',
+export default defineConfig([
+  globalIgnores(['dist', 'tinymce.copy.cjs']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
       },
     },
-  ]
-  
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/prop-types': 'off',
+    },
+  },
+])
--- resource/vite.config.js
+++ resource/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+    plugins: [react()],
     base: '/alight-admin/',
     build: {
         manifest: true,
     },
-    plugins: [react()],
     server: {
         cors: true,
     },

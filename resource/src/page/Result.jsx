6dbd5b3eeeb9c2e4eef940d9e935585afb052dee
juneszh <juneszh@gmail.com>
2026-02-26 17:48:54 +0800

    fix: remove result page show message

--- resource/src/page/Result.jsx
+++ resource/src/page/Result.jsx
@@ -51,7 +51,7 @@ const Error = props => {
                     extra={global.config.status === 200 ? closeButton() : (global.config.status === 401 ? loginButton() : undefined)}
                     status={global.config.status === 200 ? 'success' : (global.config.status === 401 ? 403 : global.config.status)}
                     subTitle={global.config.status === 200 || typeof global.config.status === 'string' ? undefined : localeValue(':status_' + global.config.status)}
-                    title={global.config.status === 200 ? localeValue(':success') : (global.config.message ?? global.config.status)}
+                    title={global.config.status === 200 ? localeValue(':success') : global.config.status}
                 />
             </Col>
         </Row>
